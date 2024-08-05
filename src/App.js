a6b013488782648bcf6528b6d0aaf72c0cedf4b3
Andranik Virabyan <gmparstone99@gmail.com>
2024-08-05 06:45:46 -0800

    Final Code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 		<div className='App'>
 			<header className='App-header'>
 				<h2>
-					Time to Code <span className='small'> with</span>
+				  Countdown<span className='small'> with</span>
 				</h2>
 				<img src={logo} className='App-logo' alt='logo' />
 			</header>
--- src/components/countdown/countdown.js
+++ src/components/countdown/countdown.js
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './styles.css'
 
+const COUNTDOWN_TARGET = new Date('2024-12-31T23:59:59')
+
+const getTimeLeft = () => {
+  const totalTimeLeft = COUNTDOWN_TARGET - new Date()
+  const Days = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24))
+  const Hours = Math.floor((totalTimeLeft / (1000 * 60 * 60)) % 24)
+  const Minutes = Math.floor(totalTimeLeft / (1000 * 60) % 60)
+  const Seconds = Math.floor((totalTimeLeft / 1000) % 60)
+  return { Days, Hours, Minutes, Seconds }
+}
+
 const Countdown = () => {
+  const [timeLeft, setTimeLeft] = useState(() => getTimeLeft())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(getTimeLeft())
+    }, 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
   return (
-    <div className='countdown'>
-      <h2>Countdown</h2>
+    <div className="countdown">
+      <h2> Before New Year </h2>
       <div className="content">
-        <div className="box">
-			<div className='value'><span>30</span></div>
-			<span className='label'>days</span>
-		</div>
-        <div className="box">
-			<div className='value'><span>10</span></div>
-			<span className='label'>hours</span>
-		</div>
-        <div className="box">
-			<div className='value'><span>30</span></div>
-			<span className='label'>minutes</span>
-		</div>
-        <div className="box">
-			<div className='value'><span>30</span></div>
-			<span className='label'>seconds</span>
-		</div>
+        {Object.entries(timeLeft).map((el) => {
+          const label = el[0]
+          const value = el[1]
+          return (
+            <div className="box" key={label}>
+              <div className="value">
+                <span className='text'>{value}</span>
+              </div>
+              <span className="label">{label}</span>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
